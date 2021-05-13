315929ce6eb8e2e9db14741548be0507289bc94f
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-05-13 16:28:14 -0500

    removed root directory

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const app =  express()
 const PORT = 8081
-const root = '/Users/Code/OneDrive/Rice/GitHub_School/Homework/homework_week_11/homework_week_11/Develop/public'
 
 app.get('/', (req, res) => {
    res.sendFile(path.resolve(__dirname + '/../../index.html'))
