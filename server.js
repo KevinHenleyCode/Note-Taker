893a9d493dda1f6116af54ffa4e4c083c9f7734f
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-05-13 22:16:07 -0500

    updated server.js

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app =  express()
 const PORT = 8080
 
 app.get('/', (req, res) => {
-   res.sendFile(path.resolve(__dirname + '/../../../../index.html'))
+   res.sendFile(path.resolve(__dirname + '/index.html'))
+})
+
+app.get('/', (req, res) => {
+   console.log(`${req} Works`)
 })
 
 app.get('/notes', (req, res) => {
-   res.sendFile(path.resolve(__dirname + '/../../../../notes.html'))
+   res.sendFile(path.resolve(__dirname + '/notes.html'))
 })
 
 app.listen(PORT, () => console.log(`Port: ${PORT} has started.`))
