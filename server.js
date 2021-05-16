a4e59423196f0ddc4baa6b16c836a525410cde45
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-05-15 23:10:58 -0500

    added error catching

--- server.js
+++ server.js
@@ -2,22 +2,37 @@ const express = require('express')
 const fs = require('fs')
 const id = require('uuid')
 const path = require('path')
-const app =  express()
+// const { dirname } = require('node:path')
+const app = express()
 const PORT = 8080
+
+
+
+
 
 // is a middleware parser
 app.use(express.json())
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/Develop/public/assets/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/Develop/public/assets/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 
 app.get('/api/notes', (req, res) => {
-   let notes = fs.readFileSync(path.join(__dirname, '/Develop/db/db.json'))
+   let notes = fs.readFile(path.join(__dirname, '/db/db.json'), (error) =>
+      error ? console.log(error) : console.log('No error'))
    let notesParse = JSON.parse(notes)
    res.json(notesParse)
-   // res.json(path.resolve(__dirname + '/Develop/db/db.json'))
+})
+
+app.post('/api/notes', (req, res) => {
+   let notesAdd = (fs.readFile(path.join(__dirname, '/db/db.json'), (error) => 
+   error ? console.log(error) : console.log('No error')))
+   let notesParse = JSON.parse(notesAdd)
+   notesAdd.push(req.body.id.v4())
+   fs.writeFile('/db/db.json', JSON.stringify(notesAdd), (error) => 
+   error? console.log(error) : console.log('Note added!'))
+   res.json(notesAdd)
 })
 
 app.listen(process.env.PORT || PORT, () => console.log(`Port: ${PORT} has started.`))
