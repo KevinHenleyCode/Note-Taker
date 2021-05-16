79a547e701d4f0ab8f839590c14247f3882732d5
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-05-15 20:21:17 -0500

    added get and post methods

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express')
+const fs = require('fs')
+const id = require('uuid')
 const path = require('path')
 const app =  express()
 const PORT = 8080
 
-app.get('/', (req, res) => {
-   res.sendFile(path.resolve(__dirname + '/index.html'))
-})
+// is a middleware parser
+app.use(express.json())
 
-app.get('/', (req, res) => {
-   console.log(`${req} Works`)
-})
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/Develop/public/assets/index.html')));
 
-app.get('/notes', (req, res) => {
-   res.sendFile(path.resolve(__dirname + '/notes.html'))
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/Develop/public/assets/notes.html')));
+
+
+app.get('/api/notes', (req, res) => {
+   let notes = fs.readFileSync(path.join(__dirname, '/Develop/db/db.json'))
+   let notesParse = JSON.parse(notes)
+   res.json(notesParse)
+   // res.json(path.resolve(__dirname + '/Develop/db/db.json'))
 })
 
 app.listen(process.env.PORT || PORT, () => console.log(`Port: ${PORT} has started.`))
-
