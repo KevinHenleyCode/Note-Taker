0c98f0dbc85ca1e8aaaab3f417c2e6baca7b72ee
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-05-15 23:17:30 -0500

    added comments

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const fs = require('fs')
 const id = require('uuid')
 const path = require('path')
-// const { dirname } = require('node:path')
 const app = express()
 const PORT = 8080
 
@@ -13,18 +12,22 @@ const PORT = 8080
 // is a middleware parser
 app.use(express.json())
 
+// gets index file when / is added to the url
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
+
+// gets notes file when /notes is added to the url
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-
+// gets the json objects when /api/notes is added to the url
 app.get('/api/notes', (req, res) => {
    let notes = fs.readFile(path.join(__dirname, '/db/db.json'), (error) =>
-      error ? console.log(error) : console.log('No error'))
+   error ? console.log(error) : console.log('No error'))
    let notesParse = JSON.parse(notes)
    res.json(notesParse)
 })
 
+// sends the json objects when /api/notes is added to the url
 app.post('/api/notes', (req, res) => {
    let notesAdd = (fs.readFile(path.join(__dirname, '/db/db.json'), (error) => 
    error ? console.log(error) : console.log('No error')))
@@ -35,4 +38,5 @@ app.post('/api/notes', (req, res) => {
    res.json(notesAdd)
 })
 
+// listens for the default port number or the one assigned by heroku
 app.listen(process.env.PORT || PORT, () => console.log(`Port: ${PORT} has started.`))
